de57b47644d36a649a7b1a557964f574cac3380b
ittimeline <ittimeline>
2021-10-10 08:58:37 +0800

    欢迎加入

---
+++ java-foundational/src/main/java/com/tzagri/java/foundational/object/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.tzagri.java.foundational.object;
+
+/**
+ * @author tzyangbin
+ * @version 2021/10/10 8:55
+ * @since jdk11
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("欢迎加入java");
+    }
+}
